8be2347c3bbceea19791bbf46b16f40aac6ad6f7
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2019-04-14 01:27:27 +0100

    build

---
+++ webpack.common.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    publicPath: "/",
+    filename: "main.js"
+  },
+  devtool: "source-map",
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "src/")
+    },
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              config: {
+                path: "./postcss.config.js"
+              },
+              plugins: loader => [require("autoprefixer")()]
+            }
+          },
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpg|svg|gif|mp3|mp4|ttf|eot|woff)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "./assets/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "main.css"
+    })
+  ]
+};
