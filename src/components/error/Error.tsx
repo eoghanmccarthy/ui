615901ecca506c1f13e47fbe25d0e7be0d0dd75f
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2019-04-27 23:43:22 +0100

    add error messgae

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,31 +1,51 @@
-import React, { Component } from "react";
+import React from "react";
+import classNames from "classnames";
 
-interface Props {}
+import styles from "./styles";
 
-export default class ErrorBoundary extends Component<Props, {}> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasError: false
-    };
-  }
-
-  static getDerivedStateFromError(error) {
-    return { hasError: true };
-  }
-
-  componentDidCatch(error, info) {
-    console.log(error, info);
-  }
-
-  render() {
-    const { hasError } = this.state;
-    const { children } = this.props;
-
-    if (hasError) {
-      return <span>Something went wrong.</span>;
-    }
-
-    return children;
-  }
+interface Props {
+  id?: string;
+  className?: string;
+  error?: { code?: number; message?: string };
+  message?: string;
 }
+
+const Error: React.FunctionComponent<Props> = ({
+  children,
+  id = undefined,
+  className = undefined,
+  error = {
+    code: null,
+    message: ""
+  },
+  message = ""
+}) => {
+  const _classNames = classNames("error", className);
+
+  const _getErrorMessage = (code: number) => {
+    switch (code) {
+      case 400:
+        return "No items detected";
+      case 404:
+        return "Page not found :(";
+      case 500:
+        return "Error";
+      default:
+        return "Something went wrong";
+    }
+  };
+
+  return (
+    <div id={id} className={_classNames} css={styles}>
+      <h3>Oops!</h3>
+      {message ? (
+        <span>{message}</span>
+      ) : error.code ? (
+        <span>{_getErrorMessage(error.code)}</span>
+      ) : null}
+      {children}
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/components/error/styles.ts
@@ -0,0 +1,13 @@
+import { css } from "@emotion/core";
+
+export default () => css`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 15px 0;
+
+  h3 {
+    margin-top: 0;
+  }
+`;
