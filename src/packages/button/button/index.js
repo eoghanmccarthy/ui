c296791a2890b15e56cb09ed08e81787625935cc
Eoghan McCarthy <eoghanmccarthy@outlook.com>
2020-05-15 01:44:37 +0100

    button sizing

--- src/packages/button/button/index.js
+++ src/packages/button/button/index.js
@@ -29,8 +29,8 @@ const Button = forwardRef(
         disabled={disabled}
         className={cx("ui-button", className)}
         css={css`
-          background-color: transparent;
-          min-width: ${shape === "circle" && `${size}px`};
+          min-width: ${(shape === "square" || shape === "circle") &&
+            `${size}px`};
           min-height: ${size}px;
           padding: 0 ${shape === "circle" ? "0" : "0.75em"};
           border-radius: ${borderRadius[shape]};
@@ -52,10 +52,11 @@ Button.propTypes = {
   href: string,
   target: oneOf(["_blank", "_self", "_parent", "_top"]),
   size: oneOf([24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72]),
-  shape: oneOf(["rounded", "circle", "capsule"])
+  shape: oneOf(["square", "rounded", "circle", "capsule"])
 };
 
 const borderRadius = {
+  square: "0",
   rounded: "2px",
   circle: "100%",
   capsule: "1000px"
