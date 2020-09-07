eecd9cd87930265a308d8d3e874292a0aa94c9a5
Eoghan McCarthy <eoghanmccarthy@outlook.com>
2020-09-08 00:28:38 +0100

    fix export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { Button } from "packages/button";
+export { Anchor, Button } from "packages/button";
 export { Dialog } from "packages/dialog";
 export { ErrorBoundary } from "packages/errorBoundary";
